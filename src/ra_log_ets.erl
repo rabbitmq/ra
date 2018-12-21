439ddba216915eb2a52d205cf44dde12983cb58a
kjnilsson <knilsson@pivotal.io>
2018-12-21 11:24:15 +0000

    Delete mem tables outside of ra process
    Large Mem tables (ETS) can take quite a bit of time to delete. This
    change moves the ETS deletion from the ra process to the ETS owner
    process, thus avoiding potential blocking inside Ra.

--- src/ra_log_ets.erl
+++ src/ra_log_ets.erl
@@ -3,7 +3,8 @@
 -behaviour(gen_server).
 
 -export([start_link/1,
-         give_away/1]).
+         give_away/1,
+         delete_tables/1]).
 
 -export([init/1,
          handle_call/3,
@@ -29,6 +30,10 @@ start_link(DataDir) ->
 give_away(Tid) ->
     ets:give_away(Tid, whereis(?MODULE), undefined).
 
+-spec delete_tables([ets:tid()]) -> ok.
+delete_tables(Tids) ->
+    gen_server:cast(?MODULE, {delete_tables, Tids}).
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
@@ -51,13 +56,29 @@ init([DataDir]) ->
     %% {RaUId, ra_index()}
     _ = ets:new(ra_log_snapshot_state, [set | TableFlags]),
 
-    ra_directory:init(DataDir),
+    ok = ra_directory:init(DataDir),
     {ok, #state{}}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+handle_cast({delete_tables, Tids}, State) ->
+    %% delete ets tables,
+    %% we need to be defensive here.
+    %% it is better to leak a table than to crash them all
+    [begin
+         try ets:delete(Tid) of
+             true -> ok
+         catch
+             _:Err ->
+                 ?WARN("ra_log_ets: failed to delete ets table ~w with ~w "
+                       "This table may need to be cleaned up manually~n",
+                       [Tid, Err]),
+                 ok
+         end
+     end || Tid <- Tids],
+    {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
