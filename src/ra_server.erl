8d47eae474aba3e7849279782852c9eeaf6649fe
Diana Corbacho <diana@rabbitmq.com>
2019-04-08 23:13:22 +0100

    Rename $ra_no_return to $ra_no_reply
    [#164735591]

--- src/ra_server.erl
+++ src/ra_server.erl
@@ -1812,7 +1812,7 @@ add_next_cluster_change(Effects,
 add_next_cluster_change(Effects, State) ->
     {Effects, State}.
 
-add_reply(_, '$ra_no_return', _, Effects, Notifys) ->
+add_reply(_, '$ra_no_reply', _, Effects, Notifys) ->
     {Effects, Notifys};
 add_reply(#{from := From}, Reply, await_consensus, Effects, Notifys) ->
     {[{reply, From, {wrap_reply, Reply}} | Effects], Notifys};
