3392d2f22d52b1f6ab5106d25882474713a4fbb7
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-10-30 14:23:42 +0100

    Handle duplicated entries in WAL in ra_dbg
    Followers can overwrite somes entries, making the index going back in
    the WAL. This is an implementation details, but this can make the replay
    fail because the state machine will see the same entries twice, making
    the state potentially inconsistent. This commit keeps only the latest
    entry if some duplicated index shows up.

--- test/ra_dbg_SUITE.erl
+++ test/ra_dbg_SUITE.erl
@@ -6,40 +6,27 @@
 -include_lib("eunit/include/eunit.hrl").
 
 all() ->
-  [replay].
+  [replay, filter_entry_duplicate].
 
 init_per_suite(Config) ->
+  Config.
+
+end_per_suite(_Config) ->
+  ok.
+
+init_per_testcase(TestCase, Config) ->
   application:load(ra),
   WorkDirectory = proplists:get_value(priv_dir, Config),
-  ok = application:set_env(ra, data_dir, filename:join(WorkDirectory, "ra")),
+  ok = application:set_env(ra, data_dir, filename:join(WorkDirectory, atom_to_list(TestCase))),
   Config.
 
-end_per_suite(Config) ->
+end_per_testcase(_TestCase, _Config) ->
   application:stop(ra),
-  Config.
+  ok.
 
 replay(_Config) ->
-  %% creating a new WAL file with ra_fifo
-  Nodes = [{ra_replay, node()}],
-  ClusterId = ra_replay,
-  Config = #{name => ClusterId},
-  Machine = {module, ra_fifo, Config},
-  application:ensure_all_started(ra),
-  {ok, _, _} = ra:start_cluster(ClusterId, Machine, Nodes),
-
-  {ok, _, _} = ra:process_command(ra_replay, {enqueue, self(), 1, <<"1">>}),
-  {ok, _, _} = ra:process_command(ra_replay, {enqueue, self(), 2, <<"2">>}),
-  {ok, _, _} = ra:process_command(ra_replay, {enqueue, self(), 3, <<"3">>}),
-
-  ConsumerId = {<<"ctag1">>, self()},
-  {ok, {dequeue, {MsgId, _}}, _} = ra:process_command(ra_replay, {checkout, {dequeue, unsettled}, ConsumerId}),
-
-  {ok, _, _} = ra:process_command(ra_replay, {settle, [MsgId], ConsumerId}),
-  {ok, FinalState, _} = ra:consistent_query(ra_replay, fun(State) -> State end),
-
-  %% now replaying the WAL
-  {ok, RaDataDir} = application:get_env(ra, data_dir),
-  WalFile = filename:join([RaDataDir, node(), "00000001.wal"]),
+  {Config, FinalState} = execute_state_machine(),
+  WalFile = wal_file(),
 
   InitialState = ra_fifo:init(Config),
   Pid = spawn(?MODULE, report, [self(), 0]),
@@ -53,6 +40,39 @@ replay(_Config) ->
           end,
   5 = Count,
   ok.
+
+filter_entry_duplicate(_Config) ->
+  execute_state_machine(),
+  WalFile = wal_file(),
+
+  WalInReverseOrder = ra_log_wal:wal2list(WalFile),
+  Wal = lists:reverse(WalInReverseOrder),
+  Wal = ra_dbg:filter_entries_duplicates(lists:append(WalInReverseOrder, WalInReverseOrder)),
+  ok.
+
+execute_state_machine() ->
+  %% creating a new WAL file with ra_fifo
+  Nodes = [{ra_dbg, node()}],
+  ClusterId = ra_dbg,
+  Config = #{name => ClusterId},
+  Machine = {module, ra_fifo, Config},
+  application:ensure_all_started(ra),
+  {ok, _, _} = ra:start_cluster(ClusterId, Machine, Nodes),
+
+  {ok, _, _} = ra:process_command(ra_dbg, {enqueue, self(), 1, <<"1">>}),
+  {ok, _, _} = ra:process_command(ra_dbg, {enqueue, self(), 2, <<"2">>}),
+  {ok, _, _} = ra:process_command(ra_dbg, {enqueue, self(), 3, <<"3">>}),
+
+  ConsumerId = {<<"ctag1">>, self()},
+  {ok, {dequeue, {MsgId, _}}, _} = ra:process_command(ra_dbg, {checkout, {dequeue, unsettled}, ConsumerId}),
+
+  {ok, _, _} = ra:process_command(ra_dbg, {settle, [MsgId], ConsumerId}),
+  {ok, FinalState, _} = ra:consistent_query(ra_dbg, fun(State) -> State end),
+  {Config, FinalState}.
+
+wal_file() ->
+  {ok, RaDataDir} = application:get_env(ra, data_dir),
+  filename:join([RaDataDir, node(), "00000001.wal"]).
 
 report(Pid, Count) ->
   receive
