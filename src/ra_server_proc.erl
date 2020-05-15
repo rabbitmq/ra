ff24776180f76e4e0a61e7317b3bfe795bba93a6
Martin Björklund <mbj4668@gmail.com>
2020-05-15 13:12:19 +0200

    clarifying spec fixes for queries

--- src/ra_server_proc.erl
+++ src/ra_server_proc.erl
@@ -160,8 +160,8 @@ cast_command(ServerId, Priority, Cmd) ->
 
 -spec query(server_loc(), query_fun(),
             local | consistent | leader, timeout()) ->
-    ra_server_proc:ra_leader_call_ret(term())
-    | {ok, Reply :: term(), not_known}.
+    ra_server_proc:ra_leader_call_ret({ra_idxterm(), Reply :: term()})
+    | {ok, {ra_idxterm(), Reply :: term()}, not_known}.
 query(ServerLoc, QueryFun, local, Timeout) ->
     statem_call(ServerLoc, {local_query, QueryFun}, Timeout);
 query(ServerLoc, QueryFun, leader, Timeout) ->
