0cd73c593cdb0b6aa9ad1903898051ac70d13ce5
Karl Nilsson <kjnilsson@gmail.com>
2021-11-25 10:35:21 +0000

    Add two less safe wal write strategies.
    1. sync_after_notify: this write strategy will send confirmation notifications
    back to writing processes after writing but  _before_ initiating the fsync operation.
    
    This should improve latency for Ra machines that do not need high throughput.
    
    2. Sync Method: none: This sync method implements a noop for the
    sync operation effectively disabling fsync apart from when closing the
    file. Only to be used when the disk used provide additional guarantees. E.g.
    when the disk has batteries and can ensure data is synced in case of a power
    outage.

--- src/ra_file_handle.erl
+++ src/ra_file_handle.erl
@@ -14,7 +14,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--export([open/2, close/1, sync/1, datasync/1, write/2, read/2, position/2]).
+-export([open/2, close/1, sync/1, datasync/1, none/1, write/2, read/2, position/2]).
 -export([pwrite/2, pwrite/3, pread/2, pread/3]).
 
 -define(SERVER, ?MODULE).
@@ -37,6 +37,10 @@ sync(Fd) ->
 
 datasync(Fd) ->
     update(io_sync, fun() -> file:datasync(Fd) end).
+
+%% called when wal does not sync at all
+none(_Fd) ->
+    ok.
 
 write(Fd, Bytes) ->
     update(io_write, iolist_size(Bytes), fun() -> file:write(Fd, Bytes) end).
