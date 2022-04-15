044849cbff2f1ea3c80db8fcc0b82dfc7deca5e5
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2022-04-15 13:18:08 +0200

    ra: Wait for servers exit in `delete_cluster/3` if requested
    The caller can pass a flag to `delete_clsuter/3` to indicate if he wants
    the function to wait for servers to exit. He can also set the
    application environment variable
    `wait_for_servers_exit_in_delete_cluster` to define the global behavior.
    
    The default behavior is the same as the historical one, meaning the
    function doesn't wait for servers exit.

--- src/ra.erl
+++ src/ra.erl
@@ -40,6 +40,7 @@
          start_or_restart_cluster/5,
          delete_cluster/1,
          delete_cluster/2,
+         delete_cluster/3,
          % server management
          % deprecated
          start_server/1,
@@ -518,11 +519,30 @@ delete_cluster(ServerIds) ->
 -spec delete_cluster(ServerIds :: [ra_server_id()], timeout()) ->
     {ok, Leader :: ra_server_id()} | {error, term()}.
 delete_cluster(ServerIds, Timeout) ->
-    %% We monitor the Ra server processes to know when they exit. That's
-    %% because they can only terminate once the command below is applied.
-    %% However, we want this function to return when the Ra server processes
-    %% and their related supervision trees are really stopped.
-    MRefs = [erlang:monitor(process, ServerId) || ServerId <- ServerIds],
+    WaitForServersExit = application:get_env(
+                           ra, wait_for_servers_exit_in_delete_cluster,
+                           false),
+    delete_cluster(ServerIds, WaitForServersExit, Timeout).
+
+%% @doc Same as `delete_cluster/1' but also accepts a flag to indicate if the
+%% function should wait for servers exit and a timeout.
+%% @see delete_cluster/1
+%% @end
+-spec delete_cluster(ServerIds :: [ra_server_id()], boolean(), timeout()) ->
+    {ok, Leader :: ra_server_id()} | {error, term()}.
+delete_cluster(ServerIds, WaitForServersExit, Timeout) ->
+    %% If requested, we monitor the Ra server processes to know when they exit.
+    %% That's because they can only terminate once the command below is
+    %% applied. However, the caller may want this function to return when the
+    %% Ra server processes and their related supervision trees are really
+    %% stopped.
+    MRefs = case WaitForServersExit of
+                true ->
+                    [erlang:monitor(process, ServerId)
+                     || ServerId <- ServerIds];
+                false ->
+                    []
+            end,
     DeleteCmd = {'$ra_cluster', delete, await_consensus},
     case ra_server_proc:command(ServerIds, DeleteCmd, Timeout) of
         {ok, _, Leader} ->
