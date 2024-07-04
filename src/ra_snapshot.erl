2fbb7a32fe3d177d6d7fdfba1cefd30ff218d31a
Karl Nilsson <kjnilsson@gmail.com>
2024-07-04 11:36:41 +0100

    Avoid using file server

--- src/ra_snapshot.erl
+++ src/ra_snapshot.erl
@@ -349,7 +349,8 @@ begin_snapshot(#{index := Idx, term := Term} = Meta, MacRef, SnapKind,
 -spec promote_checkpoint(Idx :: ra_index(), State0 :: state()) ->
     {boolean(), State :: state(), Effects :: [effect()]}.
 promote_checkpoint(PromotionIdx,
-                   #?MODULE{module = Mod,
+                   #?MODULE{uid = UId,
+                            module = Mod,
                             snapshot_directory = SnapDir,
                             checkpoint_directory = CheckpointDir,
                             checkpoints = Checkpoints0} = State0) ->
@@ -367,7 +368,8 @@ promote_checkpoint(PromotionIdx,
                                 %% sync the checkpoint before promoting it
                                 %% into a snapshot.
                                 ok = Mod:sync(Checkpoint),
-                                ok = file:rename(Checkpoint, Snapshot),
+                                ok = prim_file:rename(Checkpoint, Snapshot),
+                                true = ets:insert(?ETSTBL, {UId, Idx}),
                                 Self ! {ra_log_event,
                                         {snapshot_written,
                                          {Idx, Term}, snapshot}}
