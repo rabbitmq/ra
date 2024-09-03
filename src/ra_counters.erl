aab0f38e9a53262e4a737ec69e7b8f2ea20642ea
Michael Davis <mcarsondavis@gmail.com>
2024-09-03 16:56:33 -0400

    Add `ra_counters:new/4` wrapping `seshat:new/4`

--- src/ra_counters.erl
+++ src/ra_counters.erl
@@ -10,6 +10,7 @@
 -export([
          init/0,
          new/2,
+         new/3,
          fetch/1,
          overview/0,
          overview/1,
@@ -31,6 +32,11 @@ init() ->
     counters:counters_ref().
 new(Name, FieldsSpec) ->
     seshat:new(ra, Name, FieldsSpec).
+
+-spec new(name(), seshat:fields_spec(), seshat:label()) ->
+    counters:counters_ref().
+new(Name, FieldsSpec, Label) ->
+    seshat:new(ra, Name, FieldsSpec, Label).
 
 -spec fetch(name()) -> undefined | counters:counters_ref().
 fetch(Name) ->
