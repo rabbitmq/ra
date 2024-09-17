121a1ad3069f72c2dfcb92efe892142f13c10bc5
Karl Nilsson <kjnilsson@gmail.com>
2024-09-17 17:18:18 +0100

    bah

--- src/ra_log_ets.erl
+++ src/ra_log_ets.erl
@@ -87,9 +87,9 @@ handle_call({mem_table_please, UId}, _From,
         _ ->
             Tid = ets:new(memtbl, [set,
                                    {write_concurrency, true},
-                                   {read_concurrency, true},
+                                   % {read_concurrency, true},
                                    {decentralized_counters, true},
-                                   % compressed
+                                   compressed,
                                    public
                                   ]),
             {reply, {ok, Tid}, State#state{memtbls = Tbls#{UId => Tid}}}
