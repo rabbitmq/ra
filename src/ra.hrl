e2475c57f28283fa2576f2dd12b711e18884ba75
Michael Klishin <michaelklishin@icloud.com>
2025-03-06 20:59:49 -0500

    Bump default timeout to 20s
    There is no such thing as the right default timeout
    but some values are too prone to false positives
    to use. 5s is one of those timeouts,
    as RabbitMQ history suggests.

--- src/ra.hrl
+++ src/ra.hrl
@@ -239,7 +239,7 @@
                                                         domain => [ra]}),
        ok).
 
--define(DEFAULT_TIMEOUT, 5000).
+-define(DEFAULT_TIMEOUT, 20000).
 
 -define(DEFAULT_SNAPSHOT_MODULE, ra_log_snapshot).
 
