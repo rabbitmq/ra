7b7c4efd69688d11b8115ce36c5ee5c1aa6bbf08
Shir Levkowitz <shir@lhaim.com>
2026-06-15 21:48:32 -0400

    comment

--- test/ra_server_SUITE.erl
+++ test/ra_server_SUITE.erl
@@ -1433,6 +1433,7 @@ append_entries_reply_success_quorum(_Config) ->
      State,
      _} = ra_server:handle_leader(Msg, State0),
     ?INFO("state ~p", [State]),
+    %% With data_commit_static_quorum_size 2, we only need one ack to commit
     #{cluster := #{N2 := #{next_index := 4, match_index := 3}}, 
      commit_index := 3} = State,
     ok.
